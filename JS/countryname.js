2989a58c6581593da8bd84a0769ad9bac5a441f2
MahnoorRehman <mahnoorrehman061@gmail.com>
2023-03-24 02:12:19 +0000

    Project Done

--- JS/countryname.js
+++ JS/countryname.js
@@ -19,38 +19,3 @@
 //     })
 // });
 
-
-
-// $.ajax({
-    //     url: 'PhP/countryName.php',
-    //     type: 'POST',
-    //     dataType: 'json',
-    //     data: {
-    //         country: $('#iso-country').val(),
-    //     },
-    //     success: function (result) {
-    //         let jsondata = JSON.stringify(result);
-    //         const data = JSON.parse(jsondata);
-    //         console.log(data);
-    //         const lat = data.data[0].annotations.DMS.lat.replace(/[^\d.-]/g, '');
-    //         const lng = data.data[0].annotations.DMS.lng.replace(/[^\d.-]/g, '') * -1;
-
-    //         var marker1 = L.marker([lat, lng]).addTo(map);
-    //         marker1.bindPopup("<b>Hello world!</b><br />" + { lat } + { lng });
-    //     },
-
-    //     error: function (jqXHR, textStatus, errorThrown) {
-    //         // your error code
-    //     }
-    // });
-
-
-
-
-
-
-
-
-
-
-    
